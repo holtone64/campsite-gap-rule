e29354627aa6e40f286d2a084b2434c95be7573d
holtone64 <jthmnnysquee64@gmail.com>
2017-02-22 10:43:45 -0700

    Moved a line of a comment in the App class back to where it should be. Somehow, the two line comment got broken up

--- src/main/java/CampsiteGapRule/App.java
+++ src/main/java/CampsiteGapRule/App.java
@@ -12,11 +12,12 @@ public class App {
 		CampsiteAvailability campsiteAvailability = new CampsiteAvailability(new AvailabilityDisplay());
 		
 		// The CampsiteAvailability object can also be initialized with a path to a JSON file or 
+		// get the JSON path from the user in the console
 //		campsiteAvailability = 
 //				new CampsiteAvailability(CampsiteAvailability.readInputFile("/test-case.json"),
 //						new AvailabilityDisplay());
 		
-		// get the JSON path from the user in the console
+
 		campsiteAvailability.setJsonObject(campsiteAvailability.getAvailabilityDisplay().queryJsonPath());
 		
 		// Send our now populated and processed list of campsites to the AvailabilityDisplay object's displayAvailableCampsites() method
